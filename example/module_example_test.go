d0a6c10e7aaa2176bbafe3f578df807e20be8ee0
Alexandre NICOLAIE <alexandre.nicolaie@gmail.com>
2017-11-25 16:12:00 -0500

    Improve test quality of module
    Add pre & post tests
    Add internal logger

--- example/module_example_test.go
+++ example/module_example_test.go
@@ -1,32 +1,38 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"testing"
 	"time"
 
 	"github.com/xunleii/fantastic-broccoli/common/types/module"
 	"github.com/xunleii/fantastic-broccoli/common/types/notification/object"
 	"github.com/xunleii/fantastic-broccoli/properties"
-	"github.com/xunleii/fantastic-broccoli/utils/plugin"
 	"github.com/xunleii/fantastic-broccoli/utils"
-	"encoding/json"
+	"github.com/xunleii/fantastic-broccoli/utils/plugin"
 )
 
-func SpecializedTestImpl(t *testing.T, nprocesses int, queue *module.NotificationQueue) {
-	notifications := queue.Notifications()
+const nProcesses = 5
+var environment = plugin.NewEnvironment(definitionFactoryImpl, preTestImpl, postTestImpl, 300*time.Millisecond)
 
-	utils.AssertEquals(t, nprocesses, len(notifications))
-	for _, notification := range notifications {
-		o := notification.Content().(*object.DataObject)
-
-		utils.AssertEquals(t, 5, len(o.Value.(string)), func(a interface{}, b interface{}) bool { return a.(int) <= b.(int) })
-		t.Logf("> Data notified : {%#v} from '%s'", o.Value, o.Module)
-	}
-}
-
-func PropertyFactoryImpl() properties.ModuleDefinition {
+func definitionFactoryImpl(_type interface{}) properties.ModuleDefinition {
 	var v interface{}
-	json.Unmarshal([]byte("{\"rpm.min\":120.5,\"rpm.max\":325.0,\"rpm.step\":50,\"rpm.precision\":100}"), &v)
+
+	switch _type.(type) {
+	// For testing engine
+	case *testing.T:
+		json.Unmarshal([]byte("{\"rpm.min\":120,\"rpm.max\":500,\"rpm.step\":25,\"rpm.precision\":10}"), &v)
+	case *testing.B:
+		json.Unmarshal([]byte("{\"rpm.min\":0,\"rpm.max\":1200.0,\"rpm.step\":250,\"rpm.precision\":1000}"), &v)
+
+	// For pre testing
+	case string:
+		json.Unmarshal([]byte(_type.(string)), &v)
+
+	default:
+		log.Fatalf("unknown %#v, impossible to generate module definition", v)
+	}
 
 	return properties.ModuleDefinition{
 		Name: "RPM Generator",
@@ -34,10 +40,40 @@ func PropertyFactoryImpl() properties.ModuleDefinition {
 	}
 }
 
+
+func preTestImpl(t *testing.T, log plugin.InternalLogger, module module.Module) {
+	failure_l58 := definitionFactoryImpl("{\"rpm.max\":1200}")
+	failure_l63 := definitionFactoryImpl("{\"rpm.min\":\"0\",\"rpm.max\":\"1200\",\"rpm.step\":\"250\",\"rpm.precision\":\"1000\"}")
+	failure_l71 := properties.ModuleDefinition{Conf: nil}
+	failure_l74 := definitionFactoryImpl("{}")
+
+	// failure at l.58
+	utils.AssertNotEquals(t, nil, module.Configure(failure_l58))
+	// failure at l.63
+	utils.AssertNotEquals(t, nil, module.Configure(failure_l63))
+	// failure at l.71
+	utils.AssertNotEquals(t, nil, module.Configure(failure_l71))
+	// failure at l.74
+	utils.AssertNotEquals(t, nil, module.Configure(failure_l74))
+}
+
+func postTestImpl(t *testing.T, log plugin.InternalLogger, nprocesses int, module module.Module, queue *module.NotificationQueue) {
+	notifications := queue.Notifications()
+
+	utils.AssertEquals(t, nprocesses, len(notifications))
+	for _, notification := range notifications {
+		o := notification.Content().(*object.DataObject)
+
+		utils.AssertEquals(t, 5, len(o.Value.(string)), func(a interface{}, b interface{}) bool { return a.(int) <= b.(int) })
+		log("data notified : {%#v} from '%s'", o.Value, o.Module)
+	}
+}
+
+
 func TestModule(t *testing.T) {
-	plugin.Test(t, ExportModule(), PropertyFactoryImpl, SpecializedTestImpl, 5, 300*time.Millisecond)
+	plugin.Test(t, ExportModule(), nProcesses, environment)
 }
 
 func TestBenchmarkModule(t *testing.T) {
-	plugin.Benchmark(t, ExportModule(), PropertyFactoryImpl, 300*time.Millisecond)
+	plugin.Benchmark(t, ExportModule(), environment)
 }
--- utils/plugin/plugin.go
+++ utils/plugin/plugin.go
@@ -9,32 +9,51 @@ import (
 	"github.com/xunleii/fantastic-broccoli/log"
 	"github.com/xunleii/fantastic-broccoli/properties"
 	"github.com/xunleii/fantastic-broccoli/utils"
+	"fmt"
 )
 
-type PropertyFactory func() properties.ModuleDefinition
-type SpecializedTest func(*testing.T, int, *module.NotificationQueue)
+type InternalLogger func(format string, a ...interface{})
+type definitionFactory func(interface{}) properties.ModuleDefinition
+type preTest func(*testing.T, InternalLogger, module.Module)
+type postTest func(*testing.T, InternalLogger, int, module.Module, *module.NotificationQueue)
 
-func Benchmark(
-	t *testing.T,
-	m module.Module,
-	pfactory PropertyFactory,
-	tick time.Duration,
-) {
+type testEnvironment struct {
+	definition definitionFactory
+	tick       time.Duration
+
+	test struct {
+		pre  preTest
+		post postTest
+	}
+}
+
+func NewEnvironment(factory definitionFactory, pre preTest, post postTest, tick time.Duration) *testEnvironment {
+	return &testEnvironment{
+		definition: factory,
+		tick:       tick,
+		test: struct {
+			pre  preTest
+			post postTest
+		}{pre: pre, post: post},
+	}
+}
+
+func Benchmark(t *testing.T, mod module.Module, env *testEnvironment) {
 	t.Logf("--- Execute Benchmark ---")
 	bench := testing.Benchmark(func(b *testing.B) {
 		t.Logf("-------------------------")
 		// Init environment
 		queue := module.NewNotificationQueue()
-		log := log.NewLogger.Dev(nil)
+		logger := log.NewLogger.Dev(nil)
 
 		// Start & Configure module
-		t.Logf("- Start module '%s'\n", m.Name())
-		if err := m.Start(queue, log); err != nil {
+		t.Logf("- Start module '%s'\n", mod.Name())
+		if err := mod.Start(queue, logger); err != nil {
 			t.Fatalf("! Failure during module starting - %s\n", err.Error())
 		}
 
-		t.Logf("- Configure module '%s'\n", m.Name())
-		if err := m.Configure(pfactory()); err != nil {
+		t.Logf("- Configure module '%s'\n", mod.Name())
+		if err := mod.Configure(env.definition(b)); err != nil {
 			t.Fatalf("! Failure during module configuration - %s\n", err.Error())
 		}
 
@@ -42,7 +61,7 @@ func Benchmark(
 		t.Logf("- Start benchmark\n")
 
 		t.Logf("\t- Start new session\n")
-		if err := m.StartSession(); err != nil {
+		if err := mod.StartSession(); err != nil {
 			t.Fatalf("! Failure during starting session - %s\n", err.Error())
 		}
 
@@ -50,61 +69,65 @@ func Benchmark(
 		t.Logf("\t- Processing loops [%d time(s)]\n", b.N)
 		b.StartTimer()
 		for i := 0; i < b.N; i++ {
-			if err := m.Process(); err != nil {
+			if err := mod.Process(); err != nil {
 				// TODO: Failure only if critical (v.1.X)
 				t.Logf("! Failure during processing - %s\n", err.Error())
 			}
-			time.Sleep(tick)
+			time.Sleep(env.tick)
 		}
 		b.StopTimer()
 
-		t.Logf("- Stop module '%s'\n", m.Name())
-		m.Stop()
+		t.Logf("- Stop module '%s'\n", mod.Name())
+		mod.Stop()
 
-		time.Sleep(tick)
+		time.Sleep(env.tick)
 		t.Logf("-------------------------")
 	})
 
 	t.Logf("--- Benchmark results ---")
 
-	realTime := bench.T - tick*time.Duration(bench.N)
+	realTime := bench.T - env.tick*time.Duration(bench.N)
 	opPerSecond := time.Second / (realTime / time.Duration(bench.N))
 	t.Logf("time: %dns\t\t\t%d ns/op (%d ops)", realTime, realTime/time.Duration(bench.N), opPerSecond)
 	t.Logf("memory: %dB\t\t\t%d allocs", bench.MemBytes, bench.MemAllocs)
 }
 
-func Test(
-	t *testing.T,
-	m module.Module,
-	pfactory PropertyFactory,
-	sptest SpecializedTest,
-	nprocesses int,
-	tick time.Duration,
-) {
+func Test(t *testing.T, mod module.Module, nprocesses int, env *testEnvironment) {
 	// Init environment
 	queue := module.NewNotificationQueue()
-	log := log.NewLogger.Dev(nil)
+	logger := log.NewLogger.Dev(nil)
+	inlog := func(format string, a ...interface{}) { t.Logf("\t> %s", fmt.Sprintf(format, a...)) }
+
+
 
 	// Start & Configure module
-	t.Logf("- Start module '%s'\n", m.Name())
-	if err := m.Start(queue, log); err != nil {
+	t.Logf("- Start module '%s'\n", mod.Name())
+	if err := mod.Start(queue, logger); err != nil {
 		t.Fatalf("! Failure during module starting - %s\n", err.Error())
 	}
+	utils.AssertEquals(t, constant.States.Started, mod.State())
 
-	utils.AssertEquals(t, constant.States.Started, m.State())
-	if err := m.Configure(pfactory()); err != nil {
+	// Start pre tests
+	if env.test.pre != nil {
+		t.Logf("- Start pre tests\n")
+		env.test.pre(t, inlog, mod)
+	}
+
+	t.Logf("- Start module '%s'\n", mod.Name())
+	if err := mod.Configure(env.definition(t)); err != nil {
 		t.Fatalf("! Failure during module configuration - %s\n", err.Error())
 	}
 
-	// Start tests
 
+
+	// Start tests
 	t.Logf("- Start tests\n")
 	// Error verification (Process without session)
 	// TODO: Explicit error knowable by the manager
 	// m.Process()
 
 	t.Logf("\t- Start new session\n")
-	if err := m.StartSession(); err != nil {
+	if err := mod.StartSession(); err != nil {
 		t.Fatalf("! Failure during starting session - %s\n", err.Error())
 	}
 	// Error verification (Session already exist)
@@ -113,29 +136,35 @@ func Test(
 
 	t.Logf("\t- Processing loops [%d time(s)]\n", nprocesses)
 	for i := 0; i < nprocesses; i++ {
-		time.Sleep(tick)
-		if err := m.Process(); err != nil {
+		time.Sleep(env.tick)
+		if err := mod.Process(); err != nil {
 			// TODO: Failure only if critical (v.1.X)
 			t.Logf("! Failure during processing - %s\n", err.Error())
 		}
 	}
 
 	t.Logf("\t- Stop session\n")
-	if err := m.StopSession(); err != nil {
+	if err := mod.StopSession(); err != nil {
 		t.Fatalf("! Failure during ending session - %s\n", err.Error())
 	}
 	// Error verification (Process without session)
 	// TODO: Explicit error knowable by the manager
 	// m.Process()
 
-	if sptest != nil {
-		t.Logf("\t- Launch specialized tests\n")
-		sptest(t, nprocesses, queue)
+
+
+	// Start post tests
+	if env.test.post != nil {
+		t.Logf("- Start post tests\n")
+		env.test.post(t, inlog, nprocesses, mod, queue)
 	}
 
-	t.Logf("- Stop module '%s'\n", m.Name())
-	if err := m.Stop(); err != nil {
+
+
+	// Stop all
+	t.Logf("- Stop module '%s'\n", mod.Name())
+	if err := mod.Stop(); err != nil {
 		t.Fatalf("! Failure during ending module - %s\n", err.Error())
 	}
-	time.Sleep(2 * tick)
+	time.Sleep(2 * env.tick)
 }
