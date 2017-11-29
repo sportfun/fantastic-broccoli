8e9894272c34a818be51228e7a6e6014f5fb4b91
Xunleii <alexandre.nicolaie@gmail.com>
2017-11-28 20:38:45 -0500

    Fix plugin issue #20312

--- example/export.go
+++ example/export.go
@@ -5,3 +5,6 @@ import "github.com/xunleii/fantastic-broccoli/common/types/module"
 func ExportModule() module.Module {
 	return &rpmGenerator{}
 }
+
+// Fix issue #20312 (https://github.com/golang/go/issues/20312)
+func main() {}
